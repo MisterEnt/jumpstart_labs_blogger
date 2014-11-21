d367f0d4f395febb7ca248fb082424544aa4d336
Vincent VanOast <vvanoast@gmail.com>
2014-11-20 16:55:03 -0800

    Tagging feature complete

--- blogger/app/models/article.rb
+++ blogger/app/models/article.rb
@@ -1,3 +1,15 @@
 class Article < ActiveRecord::Base
+	def tag_list
+		self.tags.collect do |tag|
+			tag.name
+		end.join(", ")
+	end
+	def tag_list=(tags_string)
+		tags_name = tags_string.split(",").collect{|s| s.strip.downcase}.uniq
+		new_or_found_tags = tags_name.collect { |name| Tag.find_or_create_by(name: name)}
+		self.tags = new_or_found_tags
+	end
 	has_many :comments
+	has_many :taggings
+	has_many :tags, through: :taggings
 end
